26217c29b169e7f2619d021ddd20aca6c66af460
SHREEANSH01 <160627999+SHREEANSH01@users.noreply.github.com>
2026-08-18 21:39:51 +0530

    Time: 23 ms (93.72%) | Memory: 49 MB (22.96%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -2,22 +2,33 @@ class Solution {
     public int subarraySum(int[] nums, int k) {
         //prefix sum store in a map
 
-        int [] prefix=new int[nums.length];
-        prefix[0]=nums[0];
-        for(int i=1;i<nums.length;i++){
-            prefix[i]=prefix[i-1]+nums[i];
-        }
+        // int [] prefix=new int[nums.length];
+        // prefix[0]=nums[0];
+        // for(int i=1;i<nums.length;i++){
+        //     prefix[i]=prefix[i-1]+nums[i];
+        // }
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        int sum=0;
         int cnt=0;
-        for(int i=0;i<nums.length;i++){
-            if(prefix[i]==k){
-                cnt++;
+        for(int num:nums){
+            sum+=num;
+            if(map.containsKey(sum-k)){
+                cnt+=map.get(sum-k);
             }
-            for(int j=0;j<i;j++){
-                if(prefix[i]-prefix[j]==k){
-                    cnt++;
-                }
-            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
+        
+        // for(int i=0;i<nums.length;i++){
+        //     if(prefix[i]==k){
+        //         cnt++;
+        //     }
+        //     for(int j=0;j<i;j++){
+        //         if(prefix[i]-prefix[j]==k){
+        //             cnt++;
+        //         }
+        //     }
+        // }
         return cnt;
     }
 }
